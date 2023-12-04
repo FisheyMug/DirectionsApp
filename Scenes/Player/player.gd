5ed5ac13a809079acf8f88568d07fadb0d211b03
Matthew <96601521+FisheyMug@users.noreply.github.com>
2023-12-04 23:01:35 +0900

    got the basic movement mechanic up and running

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+var moveable: bool = false
+var destination: Vector2
 
 
 func _input(event):
@@ -10,9 +12,22 @@ func _input(event):
 	if event.is_action_released("go_straight"):
 		print("Go straight")
 		print(global_position)
+		print(moveable)
+		if moveable:
+			global_position = destination
+			moveable = false
 
 
 
 
-func _on_move_marker_area_entered(area):
-	print (area.global_position)
+
+
+
+func _on_view_area_entered(area):
+	destination = area.position
+	print(destination)
+	moveable = true
+
+
+func _on_view_area_exited(_area):
+	moveable = false
