42ee66917ddf7eb0aba35353c5296207b68dcc1f
Matthew <96601521+FisheyMug@users.noreply.github.com>
2023-12-06 10:24:24 +0900

    got the movement system working with move and slide

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+@export var speed = 400
+var target = destination
 var moveable: bool = false
 var destination: Vector2
 
@@ -7,27 +9,27 @@ var destination: Vector2
 func _input(event):
 	if event.is_action_released("turn_right"):
 		rotate(1.57)
+		moveable = false
 	if event.is_action_released("turn_left"):
 		rotate(-1.57)
+		moveable = false
 	if event.is_action_released("go_straight"):
-		print("Go straight")
-		print(global_position)
 		print(moveable)
 		if moveable:
-			global_position = destination
+			target = destination
 			moveable = false
-
-
-
-
-
-
+			velocity = position.direction_to(target) * speed
+			
 
 func _on_view_area_entered(area):
 	destination = area.position
-	print(destination)
 	moveable = true
 
+#func _on_view_area_exited(_area):
+	#moveable = false
+	
 
-func _on_view_area_exited(_area):
-	moveable = false
+func _physics_process(_delta):
+	
+	if position.distance_to(target) > 10:
+		move_and_slide()
