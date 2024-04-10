7b35283c1efb829bc7b74c499f4f9ad9f8a13d06
Matthew <96601521+FisheyMug@users.noreply.github.com>
2024-04-10 15:40:21 +0900

    Added way to get to create a level from the menue so I can test this build

--- StartMenu.gd
+++ StartMenu.gd
@@ -11,3 +11,7 @@ func _on_og_school_pressed():
 
 func _on_new_school_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Level/this_is_my_favourite_place.tscn")
+
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Level/create_a_level.tscn")
